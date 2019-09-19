3e7518962fdafee36d8fc2b8d9352cb97ad189ca
Marcelo216 <mtegaldo216@hotmail.com>
2019-09-18 22:44:14 -0300

    Se crea aplicacion y modelos

---
+++ proyecto/aplicacion/models.py
@@ -0,0 +1,40 @@
+rom django.db import models
+
+# Create your models here.
+class Persona(models.Model):
+    Nombre = models.CharField(max_length=100)
+    Apellido = models.CharField(max_length=100)
+    Telefono = models.IntegerField(max_length=100)
+    Email = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.Nombre
+
+    class Meta:
+        verbose_name = "Persona"
+        verbose_name_plural = "Personas"
+
+class Producto(models.Model):
+    Nombre = models.CharField(max_length=100)
+    Descripcion = models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return self.Nombre
+
+    class Meta:
+        verbose_name = "Producto"
+        verbose_name_plural = "Productos"
+
+class Compra(models.Model):
+    Persona = models.ForeignKey(Persona)
+    Producto = models.ForeignKey(Producto)
+    fecha = models.DateField(_(u"Fecha de compra"), auto_now_add=True, blank=True)
+    hora = models.TimeField(_(u"Hora de compra"), auto_now_add=True, blank=True)
+
+    def __str__(self):
+        return (self.Persona, self.Producto)
+
+    class Meta:
+        verbose_name = "Compra"
+        verbose_name_plural = "Compras"
